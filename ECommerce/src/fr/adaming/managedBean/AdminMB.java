d47d6e8e8e32fa8bb0a07335402a7a94aa82d81c
inti0242 <inti0242@DESKTOP-0QCO84F.home>
2017-02-23 16:35:38 +0100

    page xhtml

--- ECommerce/src/fr/adaming/managedBean/AdminMB.java
+++ ECommerce/src/fr/adaming/managedBean/AdminMB.java
@@ -1,5 +1,45 @@
 package fr.adaming.managedBean;
 
-public class AdminMB {
+import java.io.Serializable;
 
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import fr.adaming.entities.Admin;
+import fr.adaming.metier.IAdminService;
+
+@ManagedBean(name="adminMB")
+@ViewScoped
+public class AdminMB implements Serializable{
+	
+	private Admin admin;
+	@EJB
+	private IAdminService adminService;
+
+	public AdminMB() {
+		this.admin = new Admin();
+	}
+
+	public Admin getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(Admin admin) {
+		this.admin = admin;
+	}
+
+	public IAdminService getAdminService() {
+		return adminService;
+	}
+
+	public void setAdminService(IAdminService adminService) {
+		this.adminService = adminService;
+	}
+	
+//------Methodes admin-----
+	public String ajouter(){
+		adminService.ajouterAdminService(this.admin);
+		
+	}
 }
