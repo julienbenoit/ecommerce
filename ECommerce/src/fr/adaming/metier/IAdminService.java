b6ef9dacc56c4d706e87871aecc2db3c2dfd3049
inti0293 <inti0293@DESKTOP-4C50ANM.home>
2017-02-23 17:35:16 +0100

    commit metier iAdminService

--- ECommerce/src/fr/adaming/metier/IAdminService.java
+++ ECommerce/src/fr/adaming/metier/IAdminService.java
@@ -13,10 +13,35 @@ public interface IAdminService {
 	 * This method add a product to the table product
 	 * @param p
 	 */
-	public void ajouterAdminService(Admin p);
-//	public void supprimerAdminService(Produit p);
-//	public void mofifierAdminService(Produit p);
-//	public List<Produit> consulterAdminService();
+	public void ajouterAdminService(Admin a);
+	
+	/**
+	 * This method delete the product from the table product
+	 * @param p is an object Produit (long idProduit, String designation, String description,float prix, int quantite, boolean selectionne)
+	 * @return void : this method is an action doesn't return anything
+	 */
+	public void supprimerAdminService(Produit p);
+	
+	/**
+	 * This method update the product from the table product
+	 * @param p is an object Produit (long idProduit, String designation, String description,float prix, int quantite, boolean selectionne)
+ 	 * @return void : this method is an action doesn't return anything
+	 */
+	public void mofifierAdminService(Produit p);
 	
 	
+	/**
+	 * This method get all the categories from the table product
+	 * @param void
+	 * @return List<Categorie> : return the list of all categories objects
+	 */
+	public List<Produit> consulterAdminService();
+	
+	
+	
+	
+	
+	
+
 }
+
