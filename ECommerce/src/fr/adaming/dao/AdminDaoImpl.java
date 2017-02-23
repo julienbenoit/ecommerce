950752fe6f08332b8e98f766316972fd4fc97086
inti0293 <inti0293@DESKTOP-4C50ANM.home>
2017-02-23 17:19:39 +0100

    commit pour confilict

--- ECommerce/src/fr/adaming/dao/AdminDaoImpl.java
+++ ECommerce/src/fr/adaming/dao/AdminDaoImpl.java
@@ -1,6 +1,24 @@
 package fr.adaming.dao;
 
-public class AdminDaoImpl {
-/////hfhgfhgfhfhfhfhfh
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import fr.adaming.entities.Admin;
+
+@Stateless
+public class AdminDaoImpl implements IAdminDao {
+
 	
-}
+		@PersistenceContext(unitName="ECommerce")
+		EntityManager em;
+		@Override
+		public void ajouterAdminDao(Admin a) {
+			em.persist(a);
+			
+		}
+		
+	}
+
+	
+
--- ECommerce/src/fr/adaming/metier/IAdminService.java
+++ ECommerce/src/fr/adaming/metier/IAdminService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import fr.adaming.entities.Produit;
 
 public interface IAdminService {
-
+	/**
+	 * This method add a product to the table product
+	 * @param p
+	 */
 	public void ajouterAdminService(Produit p);
 	public void supprimerAdminService(Produit p);
 	public void mofifierAdminService(Produit p);
